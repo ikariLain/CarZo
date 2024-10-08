d4cdbc565a583ffd322ab3bbc0b8022055e70004
mathe_xtdmb79 <mathe_xtdmb79@DESKTOP-49FG9L3>
2024-10-08 09:42:05 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CarZo.Vehicle;
+
+namespace CarZo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Uppgift 2
+            // Skapa några fordon
+            Car car = new Car("Volvo", "V60", 5);
+            Motorcycle motorcycle = new Motorcycle("Harley-Davidson", "Street 750", false);
+            Truck truck = new Truck("Scania", "R500", 3);
+
+            // Skriv ut alla fordon
+            Console.WriteLine("Alla fordon:");
+            Console.WriteLine(car);
+            Console.WriteLine(motorcycle);
+            Console.WriteLine(truck);
+
+            // Filtrera och skriv ut bara bilar
+            Console.WriteLine("\nBara bilar:");
+            if (car is Car)
+            {
+                Console.WriteLine(car);
+            }
+
+            // Kör alla fordon
+            Console.WriteLine("\nKör alla fordon:");
+            car.Drive();
+            motorcycle.Drive();
+            truck.Drive();
+
+            */
+
+            // Uppgift 3 
+
+            List<Vehicle> vehicles = new List<Vehicle>();
+
+            // Lägg till några fordon
+            vehicles.Add(new Car("Volvo", "V60", 5));
+            vehicles.Add(new Motorcycle("Harley-Davidson", "Street 750", false));
+            vehicles.Add(new Truck("Scania", "R500", 3));
+            vehicles.Add(new Car("Toyota", "Corolla", 4));
+
+            // Skriv ut alla fordon
+            Console.WriteLine("Alla fordon:");
+            foreach (var vehicle in vehicles)
+            {
+                Console.WriteLine(vehicle);
+            }
+
+            // Filtrera och skriv ut bara bilar
+            Console.WriteLine("\nBara bilar:");
+            var cars = FilterVehicles<Car>(vehicles);
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car);
+            }
+
+            // Sälj ett fordon
+            var vehicleToSell = vehicles.Find(v => v.Brand == "Toyota");
+            if (vehicleToSell != null)
+            {
+                SellVehicle(vehicles, vehicleToSell);
+            }
+
+            // Skriv ut uppdaterad lista
+            Console.WriteLine("\nUppdaterad lista efter försäljning:");
+            foreach (var vehicle in vehicles)
+            {
+                Console.WriteLine(vehicle);
+            }
+        }
+
+        static List<T> FilterVehicles<T>(List<Vehicle> vehicles) where T : Vehicle
+        {
+            return vehicles.OfType<T>().ToList();
+        }
+
+        static void SellVehicle(List<Vehicle> vehicles, Vehicle vehicle)
+        {
+            Console.WriteLine($"\nSäljer fordon: {vehicle}");
+            vehicles.Remove(vehicle);
+        }
+
+    }
+}
---
+++ Vehicle.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarZo
+{
+    public interface IDriveable
+    {
+        void Drive();
+        
+    }
+    abstract class Vehicle
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+
+        public Vehicle(string Brand, string Model)
+        {
+          this.Brand = Brand;
+          this.Model = Model;
+
+        }
+
+        public override string ToString()
+        {
+            return $"{Brand} {Model}";
+        }
+
+        public class Car : Vehicle, IDriveable 
+        {
+            public int NumberOfPassengers { get; set; }
+
+            public Car(string Brand, string Model, int NumberOfPassengers) :base (Brand, Model)
+            {
+                this.NumberOfPassengers = NumberOfPassengers;
+            }
+
+            public void Drive()
+            {
+                Console.WriteLine($"Driving the car: {Brand} {Model} with {NumberOfPassengers} passengers");
+            }
+
+            public override string ToString()
+            {
+                return $"{Brand} {Model}, {NumberOfPassengers} passengers";
+            }
+
+        }
+        public class Motorcycle : Vehicle, IDriveable
+        {
+            public bool HasSidecar { get; set; }
+
+            public Motorcycle(string Brand, string Model, bool hasSidecar) : base (Brand, Model)
+            {
+                this.HasSidecar= hasSidecar;
+            }
+
+            public void Drive ()
+            {
+                Console.WriteLine($"Riding the motorcycle: {Brand} {Model} with sidecar: {HasSidecar}");
+            }
+
+            public override string ToString()
+            {
+                return $"{Brand} {Model}, Sidecar: {(HasSidecar ? "Yes" : "No")}";
+            }
+
+
+        }
+        public class Truck : Vehicle, IDriveable
+        {
+            public int NumberOfAxles { get; set; }
+
+            public Truck(string Brand, string Model, int NumberOfAxles) : base (Brand, Model)
+            {
+               this .NumberOfAxles = NumberOfAxles;
+            }
+            
+            public void Drive()
+            {
+                Console.WriteLine($"Driving the truck: {Brand} {Model} with {NumberOfAxles} axles");
+            }
+            public override string ToString()
+            {
+                return $"{Brand} {Model}, {NumberOfAxles} axles";
+            }
+        }
+
+    }
+}
